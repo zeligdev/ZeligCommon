e9261c7da6806374d61ca141e3725d2a3f201daa
Matt <mattisbusiness@gmail.com>
2012-01-04 17:12:14 -0500

    "cluster"

--- R/cluster.formula.R
+++ R/cluster.formula.R
@@ -1,3 +1,21 @@
+#
+#
+#
 cluster.formula <- function(formula, cluster) {
+# mf$formula <- update(mf$formula, paste(". ~ . + ", paste("cluster(",mf$cluster,")")))
+# mf$cluster <- NULL
+# } else if (mf$robust)
+# #"
+# mf$formula <- update(formula, paste(". ~ . + ", paste("cluster(1:nrow(",deparse(formula[[2]]),"))")))
+
+  lhs <- deparse(formula[[2]])
+
+  new.formula <- ". ~ . + "
+  cluster.part <- "cluster(1:nrow(%s))"
+  cluster.part <- sprintf(cluster.part, deparse(formula[[2]]))
+
+
+
+
   formula
 }
